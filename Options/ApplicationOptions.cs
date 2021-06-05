6010879e8a1e73608bd82a7893ac0be7993cec20
kenichiro kimura <k-kimura@alterbooth.com>
2021-06-05 10:06:02 +0900

    Initial Commit

---
+++ Options/ApplicationOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppRunnerTestWebApplication.Options
+{
+    public class ApplicationOptions
+    {
+        public string ApplicationName { get; set; }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using AppRunnerTestWebApplication.Options;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppRunnerTestWebApplication.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public readonly string applicationName;
+
+        public IndexModel(ILogger<IndexModel> logger, IOptionsMonitor<ApplicationOptions> applicationOptionsMonitor)
+        {
+            _logger = logger;
+            applicationName = applicationOptionsMonitor?.CurrentValue?.ApplicationName ?? "default name";
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
